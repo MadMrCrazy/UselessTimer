7f2edddb924ba37b424688c6c6bec3eb9e8a16ce
MadMrCrazy <madmrcrazy@gmail.com>
2018-02-21 01:12:41 +1030

    Create main.cs
    Code needs code to work

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Created by MadMrCrazy using 5 lines of code");
+            Console.WriteLine("Press enter to start");
+            Console.Read();
+            for (int myValue = 0;; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+            
+    }
+}
